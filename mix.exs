b8b8b65d72dc538f28f51286c334dfb19d551bc6
Tobias Casper <tobias.casper@gmail.com>
2019-07-31 21:55:49 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Delx.MixProject do
   def project do
     [
       app: :delx,
-      version: "3.0.0",
+      version: "3.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
